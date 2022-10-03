8be5943517111599559359b3f016c344316688b8
Ronald Andero Õisman <ronald-andero.oisman@khk.ee>
2022-10-03 02:51:58 -0700

    As a user I can delete all tasks at once [delivers #183413813]

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
-const form = document.querySelector('form')
-const taskInput = document.querySelector('#task')
-const taskList = document.querySelector('ul')
+const form = document.querySelector("form");
+const taskInput = document.querySelector("#task");
+const taskList = document.querySelector("ul");
+const delAllTasks = document.querySelector("#del-tasks");
 
+form.addEventListener("submit", addTask);
+taskList.addEventListener("click", deleteTask);
+delAllTasks.addEventListener("click", deleteAllTasks);
 
-form.addEventListener('submit', addTask)
-taskList.addEventListener('click', deleteTask)
-
-
-function addTask(e){
-    // create list item
-    
-    const li = document.createElement('li')
-    
-    li.appendChild(document.createTextNode(taskInput.value))
-    li.className = 'collection-item'
-    
-    const a = document.createElement('a')
-    
-    a.appendChild(document.createTextNode('X'))
-    a.className = 'blue-text secondary-content'
-    a.setAttribute('href', '#')
-    
-    li.appendChild(a)
-    
-    // add to list
-    
-    const ul = document.querySelector('ul')
-    ul.appendChild(li)
-    taskInput.value = ''
-    e.preventDefault()
+function addTask(e) {
+  // create list item
+  
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(taskInput.value));
+  li.className = "collection-item";
+  const a = document.createElement("a");
+  a.appendChild(document.createTextNode("X"));
+  a.className = "blue-text text-darken-2 secondary-content";
+  a.setAttribute("href", "#");
+  li.appendChild(a);
+ 
+  // add to list
+  
+  const ul = document.querySelector("ul");
+  ul.appendChild(li);
+  taskInput.value = "";
+  e.preventDefault();
 }
 
-function deleteTask(e){
-    if(e.target.textContent == 'X'){
-        if(confirm("You sure 'bout this?")){
-            e.target.parentElement.remove()
-        }
+function deleteTask(e) {
+  if (e.target.textContent == "X") {
+    if (confirm("Are you sure to delete this task?")) {
+      e.target.parentElement.remove();
     }
+  }
+}
+
+function deleteAllTasks(e) {
+  //taskList.innerHTML = ''
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
 }
